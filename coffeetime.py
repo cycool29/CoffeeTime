797912a828df6171363d531bb85df11ec7574de5
cycool29 <cycool29@gmail.com>
2022-03-21 22:34:26 +0800

    Update

--- coffeetime.py
+++ coffeetime.py
@@ -1,3 +1,5 @@
+from doctest import master
+import threading
 import os
 os.environ['PYSTRAY_BACKEND'] = 'gtk'
 import gi
@@ -21,10 +23,10 @@ else:
     DIRECTORY = os.getenv("DIRECTORY")
 
 
-def update_current_time():
+def update_current_time_seconds():
     currentTime = datetime.datetime.now().strftime("%H:%M")
     time_label['text'] = "It's " + currentTime + ' now.\n'
-    time_frame.after(1, update_current_time)
+    time_frame.after(1, update_current_time_seconds)
 
 
 def quit_window(icon, item):
@@ -42,25 +44,60 @@ def withdraw_window():
     window.withdraw()
     menu = (item('Show', show_window, default=True), item('Quit', quit_window))
     icon = pystray.Icon("name", icon_image, "title", menu)
-    icon.visible = True
-    icon.run()
+    icon.run_detached()
 
 
 def open_url(url):
     webbrowser.open(url)
 
-
 def print_spinbox_value():
     pass
 
-def open_popup():
-    top = tk.Toplevel(window)
-    top.geometry("750x250")
-    top.title("Child Window")
-    tk.Label(top, text="Hello World!", font=(
-        'Mistral 18 bold')).place(x=150, y=80)
+def coffee_break_countdown2():
+    global timer_thread
+    timer_thread = threading.Thread(target=coffee_break_countdown, name="CoffeeTime")
+    timer_thread.start()
 
+def coffee_break_countdown():
+    countdown_time_seconds = int(time_spinbox.get())*60 # minutes to seconds 
+    original_countdown_time = countdown_time_seconds
+    current_time_seconds = int(datetime.datetime.now().strftime("%H"))*3600 + int(datetime.datetime.now().strftime("%M"))*60 # hour to seconds + minutes to seconds
+    next_break_time_seconds = current_time_seconds + countdown_time_seconds
+    next_break_time_interval_seconds  = next_break_time_seconds - current_time_seconds
+    next_break_time_interval_user = next_break_time_interval_seconds / 60
+    
+    if next_break_time_interval_user == 1:
+        lefttime_label['text'] = "\nYour next coffee break is at a minute later."
+    elif next_break_time_interval_user > 1:
+        lefttime_label['text'] = "\nYour next coffee break is at " + str(next_break_time_interval_user).split('.')[0] + " minutes later."
+    elif next_break_time_interval_user < 1:
+        lefttime_label['text'] = "\nYour next coffee break is at less than a minute later."    
+    window.update()
 
+    while True:
+        while countdown_time_seconds > 0:
+            print(countdown_time_seconds)
+            current_time_seconds = int(datetime.datetime.now().strftime("%H"))*3600 + int(datetime.datetime.now().strftime("%M"))*60
+            next_break_time_seconds = current_time_seconds + countdown_time_seconds
+            next_break_time_interval_seconds  = next_break_time_seconds - current_time_seconds
+            next_break_time_interval_user = next_break_time_interval_seconds / 60
+            
+            if next_break_time_interval_user == 1:
+                lefttime_label['text'] = "\nYour next coffee break is at a minute later."
+            elif next_break_time_interval_user > 1:
+                lefttime_label['text'] = "\nYour next coffee break is at " + str(next_break_time_interval_user).split('.')[0] + " minutes later."
+            elif next_break_time_interval_user < 1:
+                lefttime_label['text'] = "\nYour next coffee break is at less than a minute later."
+            window.update()
+            
+            countdown_time_seconds -= 1
+            time.sleep(1)
+            
+        popup_window = tix.Tk()
+        tk.Label(text='Take a coffee break.', master=popup_window).pack()
+        popup_window.mainloop()
+        countdown_time_seconds = original_countdown_time
+    
 class ImageLabel(tk.Label):
     def load(self, im):
         if isinstance(im, str):
@@ -144,85 +181,11 @@ time_unit.pack(side=tk.RIGHT)
 spinbox_frame.pack()
 
 
-def coffee_break_countdown():
-    countdown_time = int(time_spinbox.get())*60
-    start_time = (int(startfrom_hour_value.get())*3600) + (int(startfrom_minutes_value.get())*60)
-    current_time = int(datetime.datetime.now().strftime("%H"))*3600 + int(datetime.datetime.now().strftime("%M"))*60
-    
-    print('Countdown time: ' + str(countdown_time))
-    print('Start time: ' + str(start_time))
-    print('Current time: ' + str(current_time))
-    
-    next_break_time_system = start_time + countdown_time
-    next_break_time_user = str(next_break_time_system / 3600).split('.')[0] + ':' + str((next_break_time_system % 3600) / 60).split('.')[0]
-
-    while True:
-        if next_break_time_system >= current_time:
-            break
-        else:
-            next_break_time_system += countdown_time
-            print(next_break_time_system)
-        
-    next_break_time_interval_system  = next_break_time_system - current_time
-    next_break_time_interval_user = str(next_break_time_interval_system / 60).split('.')[0]   
-    
-    lefttime_label['text'] = "\nYour next coffee break is at " + next_break_time_interval_user + ' minutes later.'
-    window.update()
-
-    while True:
-        try:
-            if int(datetime.datetime.now().strftime("%H"))*3600 + int(datetime.datetime.now().strftime("%M"))*60 != next_break_time_system:
-                countdown_time = int(time_spinbox.get())*60
-                start_time = (int(startfrom_hour_value.get())*3600) + (int(startfrom_minutes_value.get())*60)
-                current_time = int(datetime.datetime.now().strftime("%H"))*3600 + int(datetime.datetime.now().strftime("%M"))*60
-                
-                print('Countdown time: ' + str(countdown_time))
-                print('Start time: ' + str(start_time))
-                print('Current time: ' + str(current_time))
-                
-                next_break_time_system = start_time + countdown_time
-                next_break_time_user = str(next_break_time_system / 3600).split('.')[0] + ':' + str((next_break_time_system % 3600) / 60).split('.')[0]
-
-                while True:
-                    if next_break_time_system >= current_time:
-                        break
-                    else:
-                        next_break_time_system += countdown_time
-                        print(next_break_time_system)
-                    
-                next_break_time_interval_system  = next_break_time_system - current_time
-                next_break_time_interval_user = str(next_break_time_interval_system / 60).split('.')[0]   
-                
-                lefttime_label['text'] = "\nYour next coffee break is at " + next_break_time_interval_user + ' minutes later.'
-                window.update()
-                time.sleep(1)
-            else:
-                sg.Window('Coffee break', [
-                            [sg.T("Hey, it's time to take a coffee break.\n")], [sg.Button(s=10, button_text='Dismiss'), sg.Button(s=10, button_text='Snooze')]]).read(close=True)
-        except:
-            pass
-
 # Start from spinbox
-startfrom_hour_value = tk.StringVar(window)
-startfrom_hour_value.set(str(datetime.datetime.now().strftime("%H")))
-startfrom_minutes_value = tk.StringVar(window)
-startfrom_minutes_value.set(str(datetime.datetime.now().strftime("%M")))
-startfrom_frame = tk.Frame(master=window)
-start_from_label = tk.Label(master=startfrom_frame,
-                            text='\nStart from:', font=default_font)
-start_from_hour_spinbox = tk.Spinbox(master=startfrom_frame, from_=0, to=24,
-                                     wrap=True, font=default_font, width=10, textvariable=startfrom_hour_value)
-start_from_separator = tk.Label(
-    master=startfrom_frame, text=':', font=default_font)
-start_from_minutes_spinbox = tk.Spinbox(master=startfrom_frame, from_=0, to=60,
-                                        wrap=True, font=default_font, width=10, textvariable=startfrom_minutes_value)
-start_button = tk.Button(master=startfrom_frame, text='Start', command=coffee_break_countdown, font=default_font)
-start_from_label.pack()
+start_frame = tk.Frame(master=window)
+start_button = tk.Button(master=start_frame, text='Start', command=coffee_break_countdown2, font=default_font)
 start_button.pack(side=tk.BOTTOM, pady=10)
-start_from_hour_spinbox.pack(side=tk.LEFT, padx=10)
-start_from_minutes_spinbox.pack(side=tk.RIGHT, padx=10)
-start_from_separator.pack(side=tk.RIGHT)
-startfrom_frame.pack()
+start_frame.pack()
 
 
 # Left time
@@ -254,6 +217,6 @@ tix.Balloon(window).bind_widget(quit_button, balloonmsg="Quit CoffeeTime.")
 
 print(time_spinbox.get())
 
-update_current_time()
+update_current_time_seconds()
 
 window.mainloop()
